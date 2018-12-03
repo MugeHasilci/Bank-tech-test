c87e9ca11b885825348c85c1b758192add685b1a
muge <muge.hasilci@hotmail.com>
2018-12-03 12:43:29 +0000

    Client can make a deposit

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,10 +1,19 @@
 require 'account'
 
 describe Account do
+  subject(:account) { described_class.new }
+
   describe "#balance" do
     it "returns the initial balance as zero" do
-      account = Account.new
       expect(account.balance).to eq 0
+    end
+  end
+
+  describe "deposit" do
+    it { is_expected.to respond_to(:deposit).with(1).argument }
+    it "adds money to the balance" do
+      account.deposit(1000)
+      expect(account.balance). to eq 1000
     end
   end
 end
